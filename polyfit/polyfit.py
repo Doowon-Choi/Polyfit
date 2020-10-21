04f68db595e18b2d0167925ccc02d6df728d70a9
Schmitz, Daniel <da.schmitz@fz-juelich.de>
2020-10-21 13:51:45 +0200

    WIP: Hawkins approach to enforce monotonicity

--- polyfit/polyfit.py
+++ polyfit/polyfit.py
@@ -9,6 +9,7 @@ import numpy as np
 import cvxpy as cv
 from sklearn.base import BaseEstimator
 from os.path import dirname
+import time
 
 PATH = dirname(__file__)
 
@@ -87,18 +88,108 @@ class PolynomRegressor(BaseEstimator):
         else:
             
             return np.nan
+
+    def check_monotonicity(self, coeffs, constraint_range):
+        '''
+        Returns roots of first derivative
+
+
+        Parameters
+        ----------
+        coeffs : ndarray
+
+        Returns
+        -------
+        roots : ndarray.
+
+        '''        
+        reversed_coeffs = coeffs[::-1]
+        
+        #calculate coefficients of 1. derivative and find roots of the polynomial    
+        derivative_coeffs = np.polyder(reversed_coeffs)
+        
+        roots = np.roots(derivative_coeffs)
+        
+        #check if roots are complex
+        complex_roots = np.all(np.iscomplex(roots))
+
+        grad_real_roots = roots[np.isreal(roots)]
+
+        if complex_roots:
+        
+            monotonic = True
+            roots_within_constraints = None
+        else:
+            
+            #check if real roots lie within the range where monotonicity\
+                #should be enforced
+            #print(real_roots)
+            roots_within_constraints_indices = (grad_real_roots > constraint_range[0]) & (grad_real_roots < constraint_range[1])
+            roots_within_constraints = grad_real_roots[roots_within_constraints_indices]
+            
+            if roots_within_constraints.size == 0:
+                
+                monotonic = True
+                
+            else:
+                
+                monotonic = False            
+        return roots_within_constraints, monotonic
     
-    def fit(self, x, y, loss = 'l2', m = 1, yrange = None, verbose = False):
+    def check_curvature(self, coeffs, constraint_range):
+        '''
+        Returns roots of second derivative
+
+
+        Parameters
+        ----------
+        coeffs : ndarray
+
+        Returns
+        -------
+        roots : ndarray.
+
+        '''        
+        reversed_coeffs = coeffs[::-1]
+        
+        #calculate coefficients of 1. derivative and find roots of the polynomial    
+        second_derivative_coeffs = np.polyder(reversed_coeffs, m = 2)
+        
+        roots = np.roots(second_derivative_coeffs)
+        
+        #check if roots are complex
+        complex_roots = np.all(np.iscomplex(roots))
+        
+        hesse_real_roots = roots[np.isreal(roots)]
+        #print("hesse real roots: ", hesse_real_roots)
+        if complex_roots:
+            
+            strict_curvature = True
+            hesse_roots_within_constraints = None
+        else:
+            
+            #check if real roots lie within the range where constraints\
+                #should be enforced
+            hesse_roots_within_constraints_indices = (hesse_real_roots > constraint_range[0]) & (hesse_real_roots < constraint_range[1])
+            hesse_roots_within_constraints = hesse_real_roots[hesse_roots_within_constraints_indices]
+            
+            if hesse_roots_within_constraints.size == 0:
+                
+                strict_curvature = True
+                
+            else:
+                
+                strict_curvature = False
+        #print("hesse roots within boundaries: ", hesse_real_roots[hesse_roots_within_constraints_indices])
+        return hesse_roots_within_constraints, strict_curvature
+        
+
+    def build_objective(self, x, y, loss = 'l2', m = 1):
         
         vander = self.vander(x)
+        #print("vander shape: ", vander.shape)
         column_norms_vander = self.column_norms(vander)
         vander = vander/column_norms_vander
-        
-        vander_grad = self.vander_grad(x)
-        vander_grad =vander_grad/column_norms_vander
-        
-        vander_hesse = self.vander_hesse(x)
-        vander_hesse = vander_hesse/column_norms_vander
         
         #set up variable for coefficients to be estimated
         
@@ -114,6 +205,7 @@ class PolynomRegressor(BaseEstimator):
             
             coeffs = cv.Variable(self.deg +1)
         
+        #print(coeffs)
         #calculate residuals
         
         residuals = vander @ coeffs -y
@@ -145,47 +237,55 @@ class PolynomRegressor(BaseEstimator):
             data_term = cv.sum(cv.huber(residuals, m))
         
         objective = cv.Minimize(data_term + self.lam * regularization_term)
-        
-        #build constraints
+
+        return objective, coeffs        
+    
+    def build_monotonic_constraints(self, coeffs, roots, column_norms):
         
         constraints = []
         
-        if self.monotonocity == 'positive':
-
-            constraints.append(vander_grad@coeffs >= 0)
-
-        elif self.monotonocity == 'negative':
-
-            constraints.append(vander_grad@coeffs <= 0)
-        
-        elif self.monotonocity is not None:
+        #vandergrad = self.vander_grad(roots)
+        for root in roots:
+            #print("constraint for root: ", root)
+            vandergrad = self.vander_grad(np.array([root]))
+            vandergrad = vandergrad.ravel()/column_norms
+            derivative = cv.sum(cv.multiply(vandergrad, coeffs))
             
-            raise ValueError("Monotonicity constraint should be " \
-                             "'positive' or 'negative'")
+            if self.monotonocity == 'positive':
+                constraints.append(derivative >= 1e-3)
+            
+            elif self.monotonocity == 'negative':
+                constraints.append(derivative <= -1e-3)
                 
-        if self.curvature == 'convex':
-
-            constraints.append(vander_hesse@coeffs >= 0)
-
-        elif self.curvature == 'concave':
-
-            constraints.append(vander_hesse@coeffs <= 0)
-            
-        elif self.curvature is not None:
-            
-            raise ValueError("Curvature constraint should be " \
-                             "'convex' or 'concave'")
+        return constraints
+    
+    def build_curvature_constraints(self, coeffs, roots, column_norms):
         
-        if yrange is not None:
+        constraints = []
+        if roots is None:
             
-            constraints.append(vander @ coeffs <= yrange[1])
-            constraints.append(vander @ coeffs >= yrange[0])
-            
-        problem = cv.Problem(objective, constraints = constraints)
+            return constraints
         
+        #vandergrad = self.vander_grad(roots)
+        for root in roots:
+            #print("constraint for root: ", root)
+            vanderhesse = self.vander_hesse(np.array([root]))
+            vanderhesse = vanderhesse.ravel()/column_norms
+            second_derivative = cv.sum(cv.multiply(vanderhesse, coeffs))
+            
+            if self.curvature == 'convex':
+                constraints.append(second_derivative >= 1e-2)
+            
+            elif self.curvature == 'concave':
+                constraints.append(second_derivative <= -1e-2)
+                
+        return constraints
+
+    def cvx_solve(self, problem, coeffs, loss, verbose):
+        print("loss: ", loss)
         try:
             
-            if loss == 'l1' or self.regularization != 'l1':
+            if loss == 'l1' or self.regularization is not None:
             #l1 loss solved by ECOS. Lower its tolerances for convergence    
                 problem.solve(abstol=1e-9, reltol=1e-9, max_iters=1000000, \
                               feastol=1e-9, abstol_inacc = 1e-7, \
@@ -212,9 +312,7 @@ class PolynomRegressor(BaseEstimator):
         
         if problem.status == 'optimal':
             
-            coefficients = coeffs.value/column_norms_vander
-
-            self.coeffs_ = coefficients
+            return problem, coeffs.value
         
         #if not try SCS optimization
         else:
@@ -228,13 +326,101 @@ class PolynomRegressor(BaseEstimator):
                 pass
         
         if problem.status == 'optimal':
-            
-            coefficients = coeffs.value/column_norms_vander
 
-            self.coeffs_ = coefficients
+            return problem, coeffs.value
+            
+            #coefficients = coeffs.value/column_norms_vander
+
+            #self.coeffs_ = coefficients        
         
-        else:
+    def fit(self, x, y, loss = 'l2', m = 1, constraint_range = None, yrange = None, verbose = False):
+        
+        vander = self.vander(x)
+        column_norms = self.column_norms(vander)
+        
+        objective, coeffs = self.build_objective(x, y, loss, m)
+        problem = cv.Problem(objective)
+        problem.solve()
+        unscaled_coeffs = coeffs.value
+        rescaled_coeffs = unscaled_coeffs/column_norms
+        #problem, coeffs = self.cvx_solve(problem, coeffs, loss, verbose)
+        
+        if constraint_range is None:
             
-            print("CVXPY optimization failed")
-
+            constraint_range = [np.amin(x), np.amax(x)]
+        
+        if self.curvature is None:
+            
+            curvature = True
+            
+        if self.monotonocity is not None:
+            
+            gradient_roots, monotonic = self.check_monotonicity(rescaled_coeffs, constraint_range)
+            hesse_roots, curva = self.check_curvature(rescaled_coeffs, constraint_range)
+            print("initial hesse root: ", hesse_roots)
+            while monotonic == False or curvature == False:
+                
+                print("monotonic: ", monotonic)
+                print("derivative roots: ", gradient_roots)
+                #print("current coefs: ", coeffs.value)
+                print("curvature: ", curvature)
+                print("hesse rots: ", hesse_roots)
+                #time.sleep(0.25)
+                monotonic_constraints = self.build_monotonic_constraints(coeffs, gradient_roots, column_norms)
+                curvature_constraints = self.build_curvature_constraints(coeffs, hesse_roots, column_norms)
+                constraints = monotonic_constraints + curvature_constraints
+                #print(constraints)
+                problem = cv.Problem(objective, constraints = constraints)
+                #print(problem)
+                problem.solve(verbose = True)#eps_abs=1e-10, eps_rel=1e-10, max_iter=10000000, \
+                              #eps_prim_inf = 1e-10, eps_dual_inf = 1e-10, verbose = verbose)
+                unscaled_coeffs = coeffs.value
+                rescaled_coeffs = unscaled_coeffs/column_norms
+                #problem, coeffs = self.cvx_solve(problem, coeffs, loss, verbose)
+                
+                gradient_roots, monotonic = self.check_monotonicity(rescaled_coeffs, constraint_range)
+                #hesse_roots, curvature = self.check_curvature(rescaled_coeffs, constraint_range)
+    
+        #rescaled_coeffs = coeffs/column_norms
+        print("cvx solution: ", rescaled_coeffs)
+        #print("hesse root: ", hesse_roots)
+        self.coeffs_ = rescaled_coeffs
+        
         return self
+
+npzfile = np.load('/home/tyrion/Development/Polyfit/polyfit/Example_Data.npz')
+x = npzfile['X']
+y = npzfile['y']
+print(x.dtype)
+print(y.dtype)
+x_plot = np.linspace(0, 100, 500)    
+DEG = 2
+VERBOSE = False
+datarange=[0, 1]
+
+np_coeffs = np.polyfit(x, y, DEG)
+#der_coeffs = np.
+print("np: ", np_coeffs)
+polyestimator = PolynomRegressor(deg=DEG, monotonocity='positive', regularization = 'l1', lam = 1e-6)#, curvature='concave')
+vander = polyestimator.vander(x_plot)
+pred_numpy = vander@np_coeffs[::-1]
+
+polyestimator.fit(x, y, loss = 'l1', constraint_range=[0, 100], verbose = False) 
+pred = polyestimator.predict(x_plot)
+
+import matplotlib.pyplot as plt
+
+f, ax = plt.subplots(1, figsize = (10, 5))
+ax.set_xlim(np.amin(x_plot), np.amax(x_plot))
+ax.set_ylim(0, 1.1)
+
+ax.scatter(x, y, c='k', s=8)
+
+ax.plot(x_plot, pred_numpy, c='r', label='Deg: 5 /Numpy Polyfit')
+ax.plot(x_plot, pred, c='b', label='Polyfit')
+ax.legend(loc='upper left', frameon=False)
+plt.subplots_adjust(top=0.99,
+bottom=0.06,
+left=0.05,
+right=0.99)
+plt.show()
